8ad22a02c3731e33f971be41d365ce6cf038b11b
mori diakite <moridiakite@hotmail.com>
2019-10-14 21:13:04 +0200

    integrated authentication with GraphQL and further integration with prisma

--- graphql-prisma/src/resolvers/Subscription.js
+++ graphql-prisma/src/resolvers/Subscription.js
@@ -1,25 +1,59 @@
+import getUserId from "../utils/getUserId";
+
 const Subscription = {
 	comment: {
 		subscribe(parent, args, ctx, info) {
-			const { pubsub, db } = ctx;
+			const { prisma } = ctx;
 			const { postId } = args;
 
-			const post = db.posts.find(post => {
-				return post.id === postId && post.published;
-			});
-
-			if (!post) {
-				throw new Error("Post not found");
-			}
-
-			return pubsub.asyncIterator(`comment ${postId}`);
+			return prisma.subscription.comment(
+				{
+					where: {
+						node: {
+							post: {
+								id: postId
+							}
+						}
+					}
+				},
+				info
+			);
 		}
 	},
 	post: {
 		subscribe(parent, args, ctx, info) {
-			const { pubsub } = ctx;
+			const { prisma } = ctx;
 
-			return pubsub.asyncIterator(`post`);
+			return prisma.subscription.post(
+				{
+					where: {
+						node: {
+							published: true
+						}
+					}
+				},
+				info
+			);
+		}
+	},
+	myPost: {
+		subscribe(parent, args, { prisma, request }, info) {
+			console.log(request);
+			const userId = getUserId(request);
+			console.log(userId);
+
+			return prisma.subscription.post(
+				{
+					where: {
+						node: {
+							author: {
+								id: userId
+							}
+						}
+					}
+				},
+				info
+			);
 		}
 	}
 };
